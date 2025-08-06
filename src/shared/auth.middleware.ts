f522ddb3776f522340ebeaeba8c4600781a1d495
Emir Kutlugün <ekutlugun@migros.com.tr>
2025-08-06 15:29:51 +0300

    jwt middleware added

---
+++ src/shared/auth.middleware.ts
@@ -0,0 +1,28 @@
+import { ForbiddenException, Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly jwtService:JwtService) {
+  }
+  use(req: Request, res: Response, next: NextFunction) {
+    //req ---> gelen request;
+    //response ---> response (client'a gidecek;
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader) {
+      throw new UnauthorizedException('Authorization header is missing');
+    }
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+      throw new UnauthorizedException('Token is missing');
+    }
+    const decoded = this.jwtService.decode(<string>token); //{username:username,role:role}
+    console.log("KULLANICI ROLÜ"  +decoded['role']);
+    if(req.method=="POST" && decoded['role']!="admin"){
+      throw new ForbiddenException('NO PERMISSION ');
+    }
+    next(); // eğer her şey yolundaysa servise devam et
+  }
+}
